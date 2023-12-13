d5096a921e5708c41b016eb62714997c59b5c4cc
ElliDaon <ekdhs2939@naver.com>
2023-12-13 10:37:38 +0900

    네이버로그인 구현, nav 마이페이지 링크 추가

--- ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/persistance/MemberService_Mapper.java
+++ ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/persistance/MemberService_Mapper.java
@@ -2,12 +2,18 @@ package com.ezen_jeonju.myapp.persistance;
 
 import com.ezen_jeonju.myapp.domain.KakaoDTO;
 import com.ezen_jeonju.myapp.domain.MemberVo;
+import com.ezen_jeonju.myapp.domain.NaverDTO;
 
 public interface MemberService_Mapper {
 	public int memberIdCheck(String memberId);
 	public int memberInsert(MemberVo mv);
 	public MemberVo memberLogin(String memberId);
+	
 	public int memberIdCheckKakao(String memberId);
 	public int KakaoMemberInsert(KakaoDTO kd);
 	public MemberVo KakaoMemberLogin(String memberId);
+	
+	public int memberIdCheckNaver(String memberId);
+	public int NaverMemberInsert(NaverDTO nd);
+	public MemberVo NaverMemberLogin(String memberId);
 }
