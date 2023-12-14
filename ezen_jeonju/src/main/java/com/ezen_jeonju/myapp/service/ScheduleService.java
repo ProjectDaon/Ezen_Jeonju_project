98fd9058ee2945fa117ba096654d469881a4b374
eogml0404 <eogml0404@naver.com>
2023-12-14 17:32:07 +0900

    Schedule backend start, mybatis

--- ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/service/ScheduleService.java
+++ ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/service/ScheduleService.java
@@ -1,5 +1,14 @@
 package com.ezen_jeonju.myapp.service;
 
+import java.util.ArrayList;
+
+import com.ezen_jeonju.myapp.domain.ScheduleRootVo;
+
+// 메소드를 정의해놓은 인터페이스
 public interface ScheduleService {
+
+	public int scheduleWrite(ScheduleRootVo sv);
+	
+	public ArrayList<ScheduleRootVo> scheduleList();
 
 }
