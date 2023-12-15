1e59648e03ca1f44a08f069ec1f6cebdf0d8a494
Minjung Kim <mjkspace@gmail.com>
2023-12-15 17:33:54 +0900

    notice

--- ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/persistance/NoticeService_Mapper.java
+++ ezen_jeonju/src/main/java/com/ezen_jeonju/myapp/persistance/NoticeService_Mapper.java
@@ -3,10 +3,12 @@ package com.ezen_jeonju.myapp.persistance;
 import java.util.ArrayList;
 
 import com.ezen_jeonju.myapp.domain.NoticeVo;
+import com.ezen_jeonju.myapp.domain.SearchCriteria;
 
 public interface NoticeService_Mapper {
 	public int noticeWrite(NoticeVo nv);
-	public ArrayList<NoticeVo> noticeList();
+	public ArrayList<NoticeVo> noticeList(SearchCriteria scri);
+	public int noticeTotalCount(SearchCriteria scri);
 	public NoticeVo noticeContents(int nidx);
 	public int noticeModify(NoticeVo nv);
 	public int noticeDelete(int nidx);
